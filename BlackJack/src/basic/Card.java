5859cd92f63c300c35872ea112ee28b11b39d5da
shaker89 <shaker.khateeb@gmail.com>
2014-11-04 20:08:27 +0200

    black jack game

---
+++ BlackJack/src/basic/Card.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class Card {
+	
+	private Suit suit;
+	private int value;
+	
+	
+	public Card(Suit suit, int value) {
+		super();
+		this.suit = suit;
+		this.value = value;
+		
+	}
+	public Suit getSuit() {
+		return suit;
+	}
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
