1aa0a5e2a0e75f872d18837488cfba0d168ea2e1
shaker89 <shaker.khateeb@gmail.com>
2014-12-20 12:24:09 +0200

    added controller test

--- BlackJack/JUnit/tests/ControlerTest.java
+++ BlackJack/JUnit/tests/ControlerTest.java
@@ -7,7 +7,67 @@ import org.junit.Test;
 public class ControlerTest {
 
 	@Test
-	public void testAddToPlayers() {
+	public void testAddPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogInValid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChickIFOverOrEqual21() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateSumOFCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShuffle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChickIFOver21() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCoinsOfPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCoinsFromPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveSystem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
 		fail("Not yet implemented");
 	}
 
