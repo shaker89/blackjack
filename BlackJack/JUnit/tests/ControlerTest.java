7efc49aa76247fef717d715940b430a2c8a50d5f
shaker89 <shaker.khateeb@gmail.com>
2014-12-20 12:18:06 +0200

    added controller test

---
+++ BlackJack/JUnit/tests/ControlerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControlerTest {
+
+	@Test
+	public void testAddToPlayers() {
+		fail("Not yet implemented");
+	}
+
+}
