7e7a5ef21743c8d7e9b58947add3e1de237ead8c
shaker89 <shaker.khateeb@gmail.com>
2014-12-20 12:14:32 +0200

    added JUnit

---
+++ BlackJack/JUnit/tests/CardTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void testSetValue() {
+		fail("Not ye t implemented");
+	}
+
+}
---
+++ BlackJack/JUnit/tests/ComputerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerTest {
+
+	@Test
+	public void testUpdateSomeOFCards() {
+		fail("Not yet implemented");
+	}
+
+}
